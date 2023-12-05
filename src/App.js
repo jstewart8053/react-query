1eb96e3071dbbba410ba89d205c06035d52ab268
jstewart8053 <jstewart8053@gmail.com>
2023-12-05 13:08:34 -0500

    added axios call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "react-query";
 import "./App.css";
 import { Home } from "./components/Home";
 import { RQSuperHeroes } from "./components/RQSuperHeroes";
 import { SuperHeroes } from "./components/SuperHeroes";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
+    <QueryClientProvider client ={queryClient}>
     <Router>
       <div>
         <nav>
@@ -28,6 +32,7 @@ function App() {
         </Routes>
       </div>
     </Router>
+    </QueryClientProvider>
   );
 }
 
--- src/components/RQSuperHeroes.js
+++ src/components/RQSuperHeroes.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
 
 export const RQSuperHeroes = () => {
-  return <div>RQSuperHeroes</div>;
+ const { isLoading, data} = useQuery("super-heroes", () => { 
+  return axios.get("http://localhost:4000/super-heroes")
+ });
+
+
+if(isLoading) {
+  return <h2>Loading...</h2>
+}
+
+<div>RQSuperHeroes</div>;
 };
 
 export default RQSuperHeroes;
